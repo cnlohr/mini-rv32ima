afe5db346957a8b1f745ad00e7a7b3be33b925b8
cnlohr <lohr85@gmail.com>
2022-10-27 18:14:37 -0700

    Bump

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # riscv_emufun
+
+
+
+## Prereq
+
+```
+sudo apt-get install gcc-multilib gcc-riscv64-unknown-elf make
+```
+
---
+++ basictest/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void PlotPixel( int x, int y, uint32_t color )
+{
+	
+}
+
+int main()
+{
+	int * i = (int*)32;
+	*i = 75;
+}
+
+
+int _start()
+{
+	main();
+}
