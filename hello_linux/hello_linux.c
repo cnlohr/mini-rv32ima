f3a3087d3d37766d8471cca64eee7265f3947cfd
cnlohr <lohr85@gmail.com>
2023-11-28 20:29:54 -0800

    Add pi demo

--- hello_linux/hello_linux.c
+++ hello_linux/hello_linux.c
@@ -3,6 +3,6 @@
 int main( int argc, char ** argv )
 {
 	printf( "Hello, world %08x\n", argc );
-	float f = 7.3f;
-	printf( "f: %f\n", f );
+	float ft = 7.3f;
+	printf( "f: %f\n", ft );
 }
---
+++ hello_linux/pi.c
@@ -0,0 +1,7 @@
+// https://crypto.stanford.edu/pbc/notes/pi/code.html
+#include <stdio.h>
+int a=10000,b,c=2800,d,e,f[2801],g;int main(){for(;b-c;)f[b++]=a/5;
+for(;d=0,g=c*2;c-=14,printf("%.4d",e+d/a),e=d%a)for(b=c;d+=f[b]*a,
+f[b]=d%--g,d/=g--,--b;d*=b);}
+
+
